e682386ee12364a2571b7beebe6103c57b407a30
Joheen Chakraborty <joheen.c@gmail.com>
2025-08-20 11:23:35 -0400

    Update generate_timings.py

--- generate_timings.py
+++ generate_timings.py
@@ -1,53 +1,96 @@
-import numpy as np, cupy as cp, os, sys, ast
+import numpy as np
+import sys
+import ast
 import kerrgeopy as kg
+from pn_trajectory import trajectory
 
-
+# Parse command line arguments
 sma = float(sys.argv[1])
 ecc = float(sys.argv[2])
 incl = float(sys.argv[3])
-a = float(sys.argv[4])
-logMbh = float(sys.argv[5])
-theta_obs = cp.array(float(sys.argv[6]))
-theta_d = cp.radians(float(sys.argv[7]))
-P_d = float(sys.argv[8])
-windows = np.array(ast.literal_eval(sys.argv[9]), ndmin=2, dtype=np.float64)
-timing_file = sys.argv[10]
-window_file = sys.argv[11]
-error_file = sys.argv[12]
-phi_r0 = 0
-phi_theta0 = 0
-phi_phi0 = 0
-n_obs = cp.array([cp.sin(theta_obs), cp.array(0), cp.cos(theta_obs)])
+phi_r0 = float(sys.argv[4])
+phi_theta0 = float(sys.argv[5])
+phi_phi0 = float(sys.argv[6])
+spin = float(sys.argv[7])
+logMbh = float(sys.argv[8])
+theta_obs = float(sys.argv[9])
+theta_d = float(sys.argv[10])
+P_d = float(sys.argv[11])
+phi_d = float(sys.argv[12])
+windows = np.array(ast.literal_eval(sys.argv[13]), ndmin=2, dtype=np.float64)
+timing_file = sys.argv[14]
+window_file = sys.argv[15]
 
-phi_d = np.radians(0)
-dt = 10 # timestep in seconds
-G = 6.6743e-8 # cgs
-Msun = 1.989e33 # g
-Mbh = cp.array(10**logMbh) * Msun
-c = 2.998e10 # cm/s
-t_g = cp.array(G * Mbh / c**3)
+# Physical constants
+G = 6.6743e-8  # cgs
+Msun = 1.989e33  # g
+c = 2.998e10  # cm/s
+dt = 10  # timestep in seconds
 
+# Derived quantities
+Mbh = 10**logMbh * Msun
+t_g = G * Mbh / c**3
+
+# Save windows
 np.savetxt(window_file, windows)
-from pn_trajectory import trajectory
-t, r, (x, y, z), lambd, P_orb = trajectory(windows, np.array([logMbh]), np.array([sma]), np.array([ecc]), np.array([incl]), np.array([a]), np.array([phi_r0]), np.array([phi_theta0]), np.array([phi_phi0]), dt)
-P_d *= P_orb[0]
 
-orbit_true = kg.StableOrbit(a, sma * (1 - ecc**2), ecc, np.cos(np.radians(incl)))
-tgeo, rgeo, thetageo, phigeo = orbit_true.trajectory((0,np.pi+phi_r0,-np.pi/2+phi_theta0,phi_phi0))
+# Get orbital period from PN trajectory
+_, _, _, lambd, P_orb = trajectory(
+    windows, 
+    np.array([logMbh]), np.array([sma]), np.array([ecc]), 
+    np.array([incl]), np.array([spin]), np.array([phi_r0]), 
+    np.array([phi_theta0]), np.array([phi_phi0]), dt
+)
+P_d *= P_orb[0].get()
 lambd = lambd.get()[0]
-tgeo = cp.array(tgeo(lambd)) * t_g
-xgeo = cp.array(rgeo(lambd) * np.sin(thetageo(lambd)) * np.cos(phigeo(lambd)))
-ygeo = cp.array(rgeo(lambd) * np.sin(thetageo(lambd)) * np.sin(phigeo(lambd)))
-zgeo = cp.array(rgeo(lambd) * np.cos(thetageo(lambd)))
-n_crs_x = cp.sin(theta_d) * cp.cos(2 * cp.pi * tgeo / P_d + phi_d)
-n_crs_y = cp.sin(theta_d) * cp.sin(2 * cp.pi * tgeo / P_d + phi_d)
-n_crs_z = cp.cos(theta_d)
-dot_product = (n_obs[0]*n_crs_x + n_obs[1]*n_crs_y + n_obs[2]*n_crs_z)
+
+# Compute exact trajectory using KerrGeoPy
+orbit_true = kg.StableOrbit(spin, sma * (1 - ecc**2), ecc, np.cos(np.radians(incl)))
+tgeo_func, rgeo_func, thetageo_func, phigeo_func = orbit_true.trajectory(
+    (0, np.pi + phi_r0, -np.pi/2 + phi_theta0, phi_phi0)
+)
+
+# Evaluate trajectory
+tgeo = tgeo_func(lambd) * t_g
+rgeo = rgeo_func(lambd)
+thetageo = thetageo_func(lambd)
+phigeo = phigeo_func(lambd)
+
+# Convert to Cartesian coordinates
+sin_theta = np.sin(thetageo)
+xgeo = rgeo * sin_theta * np.cos(phigeo)
+ygeo = rgeo * sin_theta * np.sin(phigeo)
+zgeo = rgeo * np.cos(thetageo)
+
+# Compute disk normal vector (time-dependent)
+theta_d_rad = np.radians(theta_d)
+disk_phase = 2 * np.pi * tgeo / P_d + phi_d
+n_crs_x = np.sin(theta_d_rad) * np.cos(disk_phase)
+n_crs_y = np.sin(theta_d_rad) * np.sin(disk_phase)
+n_crs_z = np.cos(theta_d_rad)
+
+# Compute distance from disk
 D_t = n_crs_x * xgeo + n_crs_y * ygeo + n_crs_z * zgeo
-sign_changes = np.where((D_t[:-1]*D_t[1:])<0)[0].get()
-model_timings = tgeo[sign_changes].get()
-shapiro_delay = -2 * np.log(rgeo(lambd) * (1 + dot_product.get())) * t_g.get()
-geometric_delay = -rgeo(lambd) * dot_product.get() * t_g.get()
-model_timings += shapiro_delay[sign_changes] + geometric_delay[sign_changes]
-np.savetxt(timing_file, model_timings)
-np.savetxt(error_file, np.full_like(model_timings, 100))
+
+# Find disk crossings with interpolation
+crossings_mask = (D_t[:-1] * D_t[1:]) < 0
+alpha = -D_t[:-1] / (D_t[1:] - D_t[:-1])
+
+# Interpolate time and position at crossings
+t_cross = tgeo[:-1] + alpha * (tgeo[1:] - tgeo[:-1])
+x_cross = xgeo[:-1] + alpha * (xgeo[1:] - xgeo[:-1])
+y_cross = ygeo[:-1] + alpha * (ygeo[1:] - ygeo[:-1])
+z_cross = zgeo[:-1] + alpha * (zgeo[1:] - zgeo[:-1])
+r_cross = rgeo[:-1] + alpha * (rgeo[1:] - rgeo[:-1])
+
+# Calculate relativistic/geometrical delays
+r_mag = np.sqrt(x_cross**2 + y_cross**2 + z_cross**2)
+theta_obs_rad = np.radians(theta_obs)
+cos_angle = (np.sin(theta_obs_rad) * x_cross + np.cos(theta_obs_rad) * z_cross) / r_mag
+
+shapiro_delay = -2 * t_g * np.log(r_cross * (1 - cos_angle))
+geometric_delay = r_cross * cos_angle * t_g
+
+# Get observed crossing times & save results
+crossings = np.where(crossings_mask, t_cross + shapiro_delay + geometric_delay, np.nan)
+np.savetxt(timing_file, crossings[~np.isnan(crossings)])
